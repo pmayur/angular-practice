81d93db3daca513cb86570d7584a2bf75816368f
Mayur <thisispatil@gmail.com>
2021-09-02 17:02:53 +0530

    update: hero component, styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { HeroesComponent } from './heroes/heroes.component';
     HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../db/mock-heroes';
+import { Hero } from '../interfaces/hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
+
 export class HeroesComponent implements OnInit {
-  hero: string = 'Windstorm';
+  heroes: Hero[] = HEROES;
+  selectedHero?: Hero;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log('ngOnInit ran !');
+  }
+
+  onSelect(hero: Hero): void {
+    console.log('Selected Hero: ',hero);
+    this.selectedHero = hero;
+  }
 }
